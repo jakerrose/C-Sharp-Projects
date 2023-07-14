cf11de0b836da33f99210f197c27f669c3e6899b
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-07-14 14:11:22 -0700

    WRITING AND RUNNING CODE IN C# ASSIGNMENT

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
