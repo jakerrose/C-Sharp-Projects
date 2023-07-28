127e89aeaaa444ef156d5f3a89a8e2a0adb31e6a
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-07-27 17:27:21 -0700

    While Statement and Do While Statement assignments

---
+++ ConsoleApp Do While Statement/ConsoleApp Do While Statement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Do_While_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess my number?");
+            int numGuess = Convert.ToInt32(Console.ReadLine());
+            bool rightGuess = numGuess == 28;
+
+            do
+            {
+                switch (numGuess)
+                {
+                    case 1:
+                        Console.WriteLine("You guessed 1. Wrong.");
+                        Console.WriteLine("Guess another number");
+                        numGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 22:
+                        Console.WriteLine("You guessed 22. You're so wrong!");
+                        Console.WriteLine("Guess another number");
+                        numGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.WriteLine("I can't believe how wrong you are.");
+                        Console.WriteLine("Guess another number");
+                        numGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 28:
+                        Console.WriteLine("OMG you guessed right!");
+                        rightGuess = true;
+                        break;
+                }
+            }
+            while (!rightGuess);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp While Statement/ConsoleApp While Statement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_While_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints question, creates variable string nameGuess, creates boolean variable rightGuess which is true when equals "Rumpelstiltskin"
+            Console.WriteLine("What's my name?");
+            string nameGuess = Console.ReadLine();
+            bool rightGuess = nameGuess == "Rumpelstiltskin";
+            
+            //creates while loop which keeps loop going when rightGuess is false
+            while (!rightGuess)
+            {
+                //Switch which takes user input as variable nameGuess and runs it through these cases
+                switch (nameGuess)
+                {
+                    case "John":
+                        Console.WriteLine("You guessed John. Try again. ");
+                        Console.WriteLine("Guess a name");
+                        nameGuess = Console.ReadLine();
+                        break;
+                    default:
+                        Console.Write("Wrong name. Try again. ");
+                        Console.WriteLine("Guess a name");
+                        nameGuess = Console.ReadLine();
+                        break;
+                    case "Rumpelstiltskin":
+                        Console.Write("You guessed my name! You win!");
+                        nameGuess = Console.ReadLine();
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
