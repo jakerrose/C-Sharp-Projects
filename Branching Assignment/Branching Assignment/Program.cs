2122b88a0b593cdc00693aa8d5681b9d813e03c0
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-07-31 20:00:56 -0700

    Branching assignment resubmit

--- Branching Assignment/Branching Assignment/Program.cs
+++ Branching Assignment/Branching Assignment/Program.cs
@@ -22,24 +22,25 @@ namespace Branching_Assignment
             else
             //user will input info for width, height, and length of package
             {
-            Console.WriteLine("Please enter the width of the package in inches to the nearest inch:"); }
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the height of the package to the nearest inch:");
-            int height = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the length of the package to the nearest inch:");
-            int length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the width of the package in inches to the nearest inch:");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the height of the package to the nearest inch:");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the length of the package to the nearest inch:");
+                int length = Convert.ToInt32(Console.ReadLine());
 
-            //creates variables for total dimensions and volume of package
-            int dimensions = (width * height * length);
-            int total = (width + length + height);
+                //creates variables for total dimensions and volume of package
+                int dimensions = (width * height * length);
+                int total = (width + length + height);
 
-            //program will end of variable total is larger than 50
-            if (total > 50)
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-            //if total is less than or equal to 50, the shipping cost will print based on the formula
-            else
-                Console.WriteLine("Your estimated shipping is $" + dimensions * pounds / 100 + ".00.");
+                //program will end of variable total is larger than 50
+                if (total > 50)
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                //if total is less than or equal to 50, the shipping cost will print based on the formula
+                else
+                    Console.WriteLine("Your estimated shipping is $" + dimensions * pounds / 100 + ".00.");
             }
             Console.ReadLine();
         }
     }
+}
