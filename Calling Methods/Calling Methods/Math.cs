0c4c987ff083bede032dfaec8627c59bf4026036
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-04 12:19:01 -0700

    Calling methods assignment

---
+++ Calling Methods/Calling Methods/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+
+    public class Math
+    {
+        public static int Mult(int a)
+        {
+            int result = (a * 8);
+            Console.WriteLine(result);
+            return result;    
+        }
+
+        public static int Add(int b)
+        {
+            int result = (b + 8);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int Subt(int c)
+        {
+            int result = (c - 8);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ Calling Methods/Calling Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Writes question for user to terminal
+            Console.WriteLine("Please enter an integer to multiply 8 to.");
+            //creates var a from user input
+            int a = Convert.ToInt32(Console.ReadLine());
+            //calls method Mult to act on user parameter a
+            Math.Mult(a);
+            Console.ReadLine();
+
+            //Writes question for user to terminal
+            Console.WriteLine("Please enter an integer to add 8 to.");
+            //creates var b from user input
+            int b = Convert.ToInt32(Console.ReadLine());
+            //calls method Add to act on user parameter b
+            Math.Add(b);
+            Console.ReadLine();
+
+            //Writes question for user to terminal
+            Console.WriteLine("Please enter an integer to subtract 8 from.");
+            //creates var c from user input
+            int c = Convert.ToInt32(Console.ReadLine());
+            //calls method Subt to act on user parameter c
+            Math.Subt(c);
+            Console.ReadLine();
+
+        }
+    }
+}
