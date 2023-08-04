a819bb19d9b02dbd9e01908658ec63957aa6cb14
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-04 15:03:21 -0700

    Main method assignment

---
+++ Main Method/Main Method/Math2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    //Created public class Math 2
+    public class Math2
+    {
+        //method Dev will take in an integer and give an integer
+        public static int Div(int a)
+        {
+            int answer = (a / 2);
+            return answer;
+        }
+
+        //method Mult will take in a decimal and give an integer
+        public static int Mult(decimal b)
+        {
+            //converts decimal type to int type
+            int answer = Convert.ToInt32(b * 2); 
+            return answer;
+        }
+
+        //method Add will take in a string and give an integer
+        public static int Add(string c)
+        {
+            //converts string type to decimal
+            int answer = Convert.ToInt32(c) + 5;
+            return answer;
+        }
+    }
+}
---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math2 math = new Math2();
+
+            int x=Math2.Div(8);
+            Console.WriteLine(x);
+
+            int y=Math2.Mult(2.3m);
+            Console.WriteLine(y);
+
+            int z=Math2.Add("4");
+            Console.WriteLine(z);
+
+            Console.ReadLine();
+        }
+        
+     }
+}
