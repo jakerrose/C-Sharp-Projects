41ec53cee0106ae04613c5e73e649a8c5bd7e1c3
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-04 20:02:44 -0700

    Method class assignment

---
+++ Method Class/Method Class/Math4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    //creates public class Math4
+    public class Math4
+    {
+        //creates void method Methos with two paramaters
+        public static void Method(int a, int b)
+        { 
+            //prints message using paramater int a
+            Console.WriteLine("Integer a divided by 2 is " + (a/2));
+            //prints int b to screen
+            Console.WriteLine("Integer b is " + b);
+        }
+
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates class
+            Math4 math = new Math4();
+
+            //passes arguments to the two parameters
+            Math4.Method(50, 20);
+            Console.ReadLine();
+
+        }
+    }
+}
