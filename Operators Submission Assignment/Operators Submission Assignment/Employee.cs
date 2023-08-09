7284c8907bfb6a5eb13056543481a9f9d7f5fb56
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-08 19:22:28 -0700

    Update Employee.cs

--- Operators Submission Assignment/Operators Submission Assignment/Employee.cs
+++ Operators Submission Assignment/Operators Submission Assignment/Employee.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace Operators_Submission_Assignment
 {
+    //creates public class Employee
     public class Employee
     {
+        //creates three properties
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int ID { get; set; }
 
+        //creates public void method to intake and display ID numbers
         public void printID()
         {
             Console.WriteLine("ID number is: " + ID);
