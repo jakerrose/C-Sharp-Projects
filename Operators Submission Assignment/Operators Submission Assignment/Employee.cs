c1123f4d29c21c72259850ebfef1aa01ddc95958
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-08 19:20:20 -0700

    Operators submission assignment

---
+++ Operators Submission Assignment/Operators Submission Assignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission_Assignment
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+        public void printID()
+        {
+            Console.WriteLine("ID number is: " + ID);
+        }
+    }
+}
---
+++ Operators Submission Assignment/Operators Submission Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates first object employee1, gives ID value 300
+            Employee employee1 = new Employee() { ID = 300 };
+            //passes argument to method
+            employee1.printID();
+            //instantiates second object employee2, gives ID value 301
+            Employee employee2 = new Employee() { ID = 301 };
+            employee2.printID();
+
+            //if else statement using overloading the operator to check if values are equal
+            if (employee1.ID == employee2.ID)
+                Console.WriteLine("The employee IDs are identical.");
+            else
+                Console.WriteLine("The employee IDs are unique.");
+            Console.ReadLine();
+        }
+    }
+}
