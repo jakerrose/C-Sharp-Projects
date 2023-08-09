7c922f618967733b94682bb292ea66663e67c193
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-08 20:55:29 -0700

    Parameters

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> list1 = new Employee<string>() { Things= "ball", "mitt"};
+            
+            foreach (string item in list1.Things)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
