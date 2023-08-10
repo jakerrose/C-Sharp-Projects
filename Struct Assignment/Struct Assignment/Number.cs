06634eb85a791aefa3c6e67f251269dd429074f6
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-10 15:41:22 -0700

    Struct Assignment submission

---
+++ Struct Assignment/Struct Assignment/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    //creates new public struct Number
+    public struct Number
+    {
+        //creates property amount, data type decimal
+        public decimal Amount { get; set; }
+    }
+}
+
---
+++ Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instatiates struct Number
+            Number numbers = new Number();
+            //gives value to object numbers.Amount
+            numbers.Amount = 1.5m;
+            //prints value to console
+            Console.WriteLine(numbers.Amount);
+
+            Console.ReadLine();
+           
+        }
+    }
+ 
+}
