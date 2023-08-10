0e6787c6ccf26b1ed3bf982c4cff027fce6a4173
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-09 19:13:15 -0700

    Parameters update

--- Parameters/Parameters/Program.cs
+++ Parameters/Parameters/Program.cs
@@ -11,21 +11,23 @@ namespace Parameters
     {
         static void Main(string[] args)
         {
-            Employee<string> list1 = new Employee<string>() { Things= "ball", "mitt"};
+            Employee<string> list1 = new Employee<string>();
+            list1.Things = new List<string>() { "ball", "bat", "mit" };
+
+            Employee<int> list2 = new Employee<int>();
+            list2.Things = new List<int>() { 1, 23, 99, 103 };
             
             foreach (string item in list1.Things)
             {
                 Console.WriteLine(item);
-                Console.ReadLine();
+                
             }
 
-
-
-
-
-
-
-
+            foreach (int item in list2.Things)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }
