32921aa5f5836b1c012ed76c089a50bfdcbb3b64
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-10 18:34:40 -0700

    Lambda resubmit

--- Lamda Submission/Lamda Submission/Program.cs
+++ Lamda Submission/Lamda Submission/Program.cs
@@ -24,30 +24,38 @@ namespace Lamda_Submission
             employeeList.Add(new Employee("Mac", " Tonite", 9));
             employeeList.Add(new Employee("Joe", " Cool", 10));
 
-            //uses foreach loop list employees with first name Joe
+            //creates second list to contain employees name Joe
+            List<Employee> list2 = new List<Employee>();
+            //foreach loop, creates new variable joe in employeelist
             foreach (var joe in employeeList)
+            {   //if first name is Joe, adds to list2
                 if (joe.FirstName == "Joe")
-                { Console.WriteLine("These are the Joes I know:  " + joe.FirstName +  joe.LastName);
+                {
+                    list2.Add(joe);
+                    //writes employees named Joe from list2
+                    Console.WriteLine("These are the Joes I know: " + joe.FirstName + joe.LastName);
                 }
-            //creates list newList using lambda expression that filters out employees with first name Joe
-            List<Employee> newList = employeeList.Where(x => (x.FirstName == "Joe")).ToList();
-            //foreach loop using newList
-            foreach (Employee emp in newList)
-            {
-                Console.WriteLine("Employees named Joe: " + emp.FirstName +  emp.LastName);
             }
-            //creates list newList2 using lambda expression that filters out employees with ID number greater than 5
-            List<Employee> newList2= employeeList.Where(x => x.ID > 5).ToList();
-            //uses foreach loop on newList2 to print list of filtered employees
-            foreach(Employee emp2 in newList2)
-            {
-                Console.WriteLine("Employees with ID no. greater than 5: " + emp2.FirstName + emp2.LastName + ", " + emp2.ID);
+                
+                //creates list newList using lambda expression that filters out employees with first name Joe
+                List<Employee> newList = employeeList.Where(x => (x.FirstName == "Joe")).ToList();
+                //foreach loop using newList
+                foreach (Employee emp in newList)
+                {
+                    Console.WriteLine("Employees named Joe: " + emp.FirstName + emp.LastName);
+                }
+                //creates list newList2 using lambda expression that filters out employees with ID number greater than 5
+                List<Employee> newList2 = employeeList.Where(x => x.ID > 5).ToList();
+                //uses foreach loop on newList2 to print list of filtered employees
+                foreach (Employee emp2 in newList2)
+                {
+                    Console.WriteLine("Employees with ID no. greater than 5: " + emp2.FirstName + emp2.LastName + ", " + emp2.ID);
+                }
+                Console.ReadLine();
+
+
             }
-            Console.ReadLine();
-
-
-
 
         }
     }
-}
+
