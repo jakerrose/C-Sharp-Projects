910cd7cd3d7c82df13c7868daa8216addf7c392f
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-10 17:15:46 -0700

    Lambda assignment

---
+++ Lamda Submission/Lamda Submission/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda_Submission
+{
+    //creates public class Employee
+    public class Employee
+    {
+        //creates three properties of Employee
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+        //creates a constructor for Employee with three properties
+        public Employee(string FirstName, string LastName, int ID)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.ID = ID;
+        }
+    }
+}
---
+++ Lamda Submission/Lamda Submission/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates class Employee and creates new list employeelist
+            List<Employee> employeeList = new List<Employee>();
+            //inputs three properties for ten employees
+            employeeList.Add(new Employee("Joe", " Biden", 1));
+            employeeList.Add(new Employee("Hunter", " Biden", 2));
+            employeeList.Add(new Employee("Joe", " Camel", 3));
+            employeeList.Add(new Employee("Fresh", " Prince", 4));
+            employeeList.Add(new Employee("Barack", " Obama", 5));
+            employeeList.Add(new Employee("Muggsy", " Bogges", 6));
+            employeeList.Add(new Employee("Wade", " Boggs", 7));
+            employeeList.Add(new Employee("Joe", " Dirt", 8));
+            employeeList.Add(new Employee("Mac", " Tonite", 9));
+            employeeList.Add(new Employee("Joe", " Cool", 10));
+
+            //uses foreach loop list employees with first name Joe
+            foreach (var joe in employeeList)
+                if (joe.FirstName == "Joe")
+                { Console.WriteLine("These are the Joes I know:  " + joe.FirstName +  joe.LastName);
+                }
+            //creates list newList using lambda expression that filters out employees with first name Joe
+            List<Employee> newList = employeeList.Where(x => (x.FirstName == "Joe")).ToList();
+            //foreach loop using newList
+            foreach (Employee emp in newList)
+            {
+                Console.WriteLine("Employees named Joe: " + emp.FirstName +  emp.LastName);
+            }
+            //creates list newList2 using lambda expression that filters out employees with ID number greater than 5
+            List<Employee> newList2= employeeList.Where(x => x.ID > 5).ToList();
+            //uses foreach loop on newList2 to print list of filtered employees
+            foreach(Employee emp2 in newList2)
+            {
+                Console.WriteLine("Employees with ID no. greater than 5: " + emp2.FirstName + emp2.LastName + ", " + emp2.ID);
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
