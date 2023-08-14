eae2dca2957928520c429543a0e6f5b19c9c6c89
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-13 20:18:34 -0700

    Input assignment

---
+++ Input Assignment/Input Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //console asks for number, can be string since no operations are being done, creates string data type
+            Console.WriteLine("Please enter a number.");
+            string num1 = Console.ReadLine();
+            //prints number to console
+            Console.WriteLine("Your number is " + num1);
+            //logs number to separate text file
+            File.WriteAllText(@"C:\Users\rosej\log1.txt", num1);
+            
+            Console.ReadLine();
+        }
+    }
+}
