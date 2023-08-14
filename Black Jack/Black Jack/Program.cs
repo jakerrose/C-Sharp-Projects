e52b38722a427bcc38784f61021dbb0ae18e871d
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-13 21:32:31 -0700

    Black Jack pre Casino

---
+++ Black Jack/Black Jack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Black_Jack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = "Here is some text";
+            //File.WriteAllText(@"C:\Users\rosej\Log.txt", text);
+
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer== "yes" || answer=="yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while(player.isActivelyPlaying && player.Balance>0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing.");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.ReadLine();
+        }
+    }
+}
