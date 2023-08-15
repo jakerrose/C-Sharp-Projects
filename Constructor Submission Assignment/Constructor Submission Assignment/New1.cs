b865f160d82db616352becd5a9a62e8be158b107
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-14 21:37:33 -0700

    Constructor Submission Assignment

---
+++ Constructor Submission Assignment/Constructor Submission Assignment/New1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission_Assignment
+{
+    class New1
+    {
+        public var id = 500;
+        public const string name= "fresh";
+        public New1() : this(0, "")
+        {
+
+        }
+        public New1(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
---
+++ Constructor Submission Assignment/Constructor Submission Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            New1 a = new New1();
+            
+            Console.WriteLine($"The prince was {a.name} even though he was {a.id} years old.");
+            Console.ReadLine();
+
+        }
+    }
+}
