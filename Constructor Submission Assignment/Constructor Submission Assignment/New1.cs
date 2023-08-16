12ab28f65811f406ef4342a41080fa6bb5021e0d
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-15 19:04:16 -0700

    CSA resubmit

--- Constructor Submission Assignment/Constructor Submission Assignment/New1.cs
+++ Constructor Submission Assignment/Constructor Submission Assignment/New1.cs
@@ -8,12 +8,16 @@ namespace Constructor_Submission_Assignment
 {
     class New1
     {
-        public var id = 500;
-        public const string name= "fresh";
+        //creates public int id equal to 500
+        public int id = 500;
+        //creates public string name equal to "fresh"
+        public string name= "fresh";
+        //creates empty constructor that takes two arguments, int and string
         public New1() : this(0, "")
         {
 
         }
+        //inputs the values id and name into the method New1
         public New1(int id, string name)
         {
             this.id = id;
--- Constructor Submission Assignment/Constructor Submission Assignment/Program.cs
+++ Constructor Submission Assignment/Constructor Submission Assignment/Program.cs
@@ -10,9 +10,12 @@ namespace Constructor_Submission_Assignment
     {
         static void Main(string[] args)
         {
-            New1 a = new New1();
-            
-            Console.WriteLine($"The prince was {a.name} even though he was {a.id} years old.");
+            //creates variable using var implying string type
+            var a = "fresh";
+            //creates a constant variable, data type int
+            const int b = 500;
+            //uses the var and const in a WriteLine
+            Console.WriteLine("The prince was " + a + "even though he was " + b + "years old.");
             Console.ReadLine();
 
         }
