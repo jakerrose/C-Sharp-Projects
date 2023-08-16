2f43ad355b725158555391de8ab92eb3632b8661
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-15 20:09:00 -0700

    Try Catch assignment

---
+++ Try Catch assignment/Try Catch assignment/Exception.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch_assignment
+{
+    //creates public class Exception1, references System.Exception
+    public class Exception1: Exception
+    {
+        //creates two methods that are overloaded so that program can reference the exception
+        public Exception1()
+            : base() { }
+        public Exception1(string message)
+            : base(message) { }
+        
+    }
+}
---
+++ Try Catch assignment/Try Catch assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Try_Catch_assignment
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            //initializes try. Console asks user for age.
+            try
+            {
+                Console.WriteLine("Please enter your age.");
+                //converts string to data type int, creates variable age
+                int age = Convert.ToInt32(Console.ReadLine());
+                //if statement, if age is less than or equal to zero, throw Exception1
+                if (age <= 0)
+                {
+                    throw new Exception1();
+                }
+                //if age is greater than zero, subract age from current year, convert to int yearBorn
+                int yearBorn = 2023 - age;
+                //prints yearBorn
+                Console.WriteLine("You were born in " + yearBorn + ".");
+                Console.ReadLine();
+            }
+            //if age is less than or equal to zero, is thrown to Exception1. Console will print message
+            catch (Exception1)
+            {
+                Console.WriteLine("Please enter an integer larger than 0.");
+                Console.ReadLine();
+            }
+            //if age is something other than an integer, is thrown to general catch, this message is printed.
+            catch
+            {
+                Console.WriteLine("Please enter only integers.");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
