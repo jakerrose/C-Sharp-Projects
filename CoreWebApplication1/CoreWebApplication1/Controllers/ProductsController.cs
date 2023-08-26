de0a03ae61322bbaf0b18376ce8274fdf9450ac2
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-25 20:08:49 -0700

    Challenge1

---
+++ CoreWebApplication1/CoreWebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
