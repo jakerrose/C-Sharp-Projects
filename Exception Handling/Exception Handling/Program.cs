77710db18eb3b67876afd08a29a00c461c8efae7
Jake Rose <72412423+omniboss@users.noreply.github.com>
2023-08-27 21:26:51 -0700

    Updates

--- Exception Handling/Exception Handling/Program.cs
+++ Exception Handling/Exception Handling/Program.cs
@@ -9,42 +9,13 @@ namespace Exception_Handling
     class Program
     {
         static void Main(string[] args)
-        //{
-        //    try
-        //    {
-        //        Console.WriteLine("Pick a number.");
-        //        int numberOne = Convert.ToInt32(Console.ReadLine());
-        //        Console.WriteLine("Pick a second number.");
-        //        int numberTwo = Convert.ToInt32(Console.ReadLine());
-        //        Console.WriteLine("Dividing the two...");
-        //        int numberThree = numberOne / numberTwo;
-        //        Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
-        //        Console.ReadLine();
-        //    }
-        //    catch (FormatException ex)
-        //    {
-        //        Console.WriteLine("Please ener a whole number");
-        //        return;
-        //    }
-        //    catch (DivideByZeroException ex)
-        //    {
-        //        Console.WriteLine("Please don't divide by zero.");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //    }
-        //    finally
-        //    {
-        //        Console.ReadLine();
-        //    }
 
-        //Creates new list of stuff
-        List<String> randomStuff = new List<string>() { "hamburger", "socks", "toys", "records", "games" };
-        //asks user to enter term to search for. Bool datatype is created to determine if item in list equals search item
-        Console.WriteLine("Would you like to search our list off stuff to see if we have what you want? Type a search term.");
-                string userTerm = Console.ReadLine();
-        bool rightGuess = randomStuff.Contains(userTerm);
+        {
+            List<String> randomStuff = new List<string>() { "hamburger", "socks", "toys", "records", "games" };
+            //asks user to enter term to search for. Bool datatype is created to determine if item in list equals search item
+            Console.WriteLine("Would you like to search our list off stuff to see if we have what you want? Type a search term.");
+            string userTerm = Console.ReadLine();
+            bool rightGuess = randomStuff.Contains(userTerm);
             //Do while loop to handle user searchs. If the item matches, the bool is true and message prints. If item does not match, it will switch to the default and the program will loop
             do
             {
@@ -81,10 +52,11 @@ namespace Exception_Handling
                         userTerm = Console.ReadLine();
                         break;
                 }
-}
-while (!rightGuess) ;
-Console.ReadLine();
-
+            }
+            while (!rightGuess);
+            Console.ReadLine();
         }
     }
+}
+    
 
